92a76800e7912a55a4d1e8810c8aa670c4d8ab6d
AllenXOD <932037526@qq.com>
2019-03-30 14:53:26 +0800

    登录表单验证规则

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="login">
     <div class="box">
-      <el-form label-width="80px" label-position="top">
-        <el-form-item label="用户名">
-          <el-input></el-input>
+      <h2>用户登录</h2>
+      <!-- model 表单数据 rules 验证规则 -->
+      <el-form label-width="80px" :model="loginForm" :rules="rules" label-position="top">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button class="my-btn" type="primary">登录</el-button>
@@ -17,30 +19,32 @@
 </template>
 
 <script>
+import "../assets/sass/login.scss";
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      // 表单数据
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      // 表单验证规则
+      rules: {
+        //   required 是否必须填写 message 提示消息 trigger 触发方式
+        username: [
+          { required: true, message: "用户名不能为空", trigger: "blur" },
+          { min: 4, max: 16, message: "长度在4~16个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "用户名不能为空", trigger: "blur" },
+          { min: 6, max: 16, message: "长度在6~16个字符", trigger: "blur" }
+        ]
+      }
+    };
+  }
 };
 </script>
 
 <style lang="scss">
-// @import url('../assets/sass/login.scss');
-.login {
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #324152;
-  .box {
-    width: 550px;
-    height: 420px;
-    padding: 35px;
-    // 怪异盒模型
-    box-sizing: border-box;
-    background-color: #fff;
-    border-radius: 10px;
-    .my-btn {
-      width: 100%;
-    }
-  }
-}
 </style>
